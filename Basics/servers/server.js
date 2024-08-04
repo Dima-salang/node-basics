698f34cc4d69f2c944df01001b21b645e0014660
Midnight <luisgabrielle1026@gmail.com>
2024-08-04 16:44:56 +0800

    servers

--- Basics/servers/server.js
+++ Basics/servers/server.js
@@ -1,10 +1,29 @@
 const http = require('http'); 
+const fs = require('fs');
 
 // create a server that takes in the request and response
 const server = http.createServer((req, res) => {
     
     console.log('request made...');
-    console.log(req);
+    console.log(req.url, req.method);
+
+    // set header content type to let the browser know what kind of response it is receiving
+    res.setHeader('Content-Type', 'text/html');
+    
+    // read the html file
+    fs.readFile('../html/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } 
+
+        res.write(data);
+        res.end();
+
+        // or alternatively, we can just
+        // res.end(data);
+    })
+
  
 });
 
