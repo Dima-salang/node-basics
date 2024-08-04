318b42d2aef2ef71479222156e8314de0c411bf9
Midnight <luisgabrielle1026@gmail.com>
2024-08-04 17:42:15 +0800

    routing and redirects

--- Basics/servers/server.js
+++ Basics/servers/server.js
@@ -1,17 +1,50 @@
 const http = require('http'); 
 const fs = require('fs');
 
+/* 
+    STATUS CODES
+
+    200 - OK
+    301 - RESOURCE MOVED
+    404 - NOT FOUND
+    500 - INTERNAL SERVER ERROR
+*/
+
 // create a server that takes in the request and response
 const server = http.createServer((req, res) => {
     
     console.log('request made...');
     console.log(req.url, req.method);
 
+    // basic routing
+    let path = "../html/";
+
+    switch (req.url) {
+        case "/":
+            path += "index.html";
+            break;
+        case "/about":
+            path += "about.html";
+            break;
+        // case of redirect
+        case "/about-me":
+            res.statusCode = 301; // resource moved
+
+            // we set the res header
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default:
+            res.statusCode = 404;
+            path += "404.html";
+            break;
+    }
+
     // set header content type to let the browser know what kind of response it is receiving
     res.setHeader('Content-Type', 'text/html');
     
     // read the html file
-    fs.readFile('../html/index.html', (err, data) => {
+    fs.readFile(path, (err, data) => {
         if (err) {
             console.log(err);
             res.end();
